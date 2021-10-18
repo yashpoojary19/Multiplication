5c69c63aec0181421685f3a48193661c0ae028a2
Yash Poojary <yashspoojary245@gmail.com>
2021-10-18 14:45:12 +0530

    Initial Commit

---
+++ Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by Yash Poojary on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
